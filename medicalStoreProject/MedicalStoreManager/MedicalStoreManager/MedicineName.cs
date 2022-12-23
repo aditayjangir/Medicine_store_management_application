858ea3a2391a64d8505894ea38331453cdb0c1e1
Aditya Jangir <iamuaj101@gmail.com>
2022-12-23 23:31:08 +0530

    made some changes in main code amd made a json file for midicine list

---
+++ medicalStoreProject/MedicalStoreManager/MedicalStoreManager/MedicineName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalStoreManager
+{
+    public class MedicineName
+    {
+        public string description { get; set; }
+        public string source { get; set; }
+        public List<string> drugs { get; set; }
+    }
+}
--- medicalStoreProject/MedicalStoreManager/MedicalStoreManager/Program.cs
+++ medicalStoreProject/MedicalStoreManager/MedicalStoreManager/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-String[] MedicineName = new string[] { "Acetaminophen", "Adderall", "Amitriptyline", "Amlodipine", "Amoxicillin", "Ativan", "Atorvastatin", "Azithromycin", "Benzonatate", "Brilinta", "Bunavail", "Buprenorphine", "Cephalexin", "Ciprofloxacin", "Citalopram", "Clindamycin", "Clonazepam", "Cyclobenzaprine", "Cymbalta", "Doxycycline", "Dupixent", "Entresto", "Entyvio", "Farxiga", "Fentanyl", "Fentanyl Patch", "Gabapentin", "Gilenya", "Humira", "Hydrochlorothiazide", "Hydroxychloroquine", "Ibuprofen", "Imbruvica", "Invokana", "Januvia", "Jardiance", "Kevzara", "Lexapro", "Lisinopril", "Lofexidine", "Loratadine", "Lyrica", "Melatonin", "Meloxicam", "Metformin", "Methadone", "Methotrexate", "Metoprolol", "Naloxone", "Naltrexone", "Naproxen", "Omeprazole", "Onpattro", "Otezla", "Ozempic", "Pantoprazole", "Prednisone", "Probuphine", "Rybelsus", "secukinumab", "Sublocade", "Tramadol", "Trazodone", "Viagra", "Wellbutrin", "Xanax", "Zubsolv", };
+using System.Text.Json;
+using MedicalStoreManager;
+string drug = File.ReadAllText(@"C:\Users\jangi\OneDrive\Desktop\medical store project\medicalStoreProject\MedicalStoreManager\MedicalStoreManager\medicineName.json");
+MedicineName? drugNames = JsonSerializer.Deserialize<MedicineName>(drug);
 Console.WriteLine("Search Medicine here...");
 String medName = Console.ReadLine();
 int flag = 0;
-foreach(string med in MedicineName)
+foreach(string med in drugNames.drugs)
 {
     if (med == medName)
     {
